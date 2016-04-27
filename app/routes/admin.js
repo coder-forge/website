7ca657aadb473b05fa8e7a4248ff9c05c67fdf97
Daithi Coombes <webeire@gmail.com>
2016-04-27 20:09:23 +0100

    meteorjs: init commit
    default meteor application
    ```bash
    meteor create coder-forge
    ```

--- app/routes/admin.js
+++
@@ -1,9 +0,0 @@
-var express = require('express')
-  ,router = express.Router()
-
-var tools = require('../lib/tools')
-
-router.get('/tools', tools.view)
-router.post('/tools', tools.upsert)
-
-module.exports = router
--- tests/app/routes/admin.spec.js
+++
@@ -1,9 +0,0 @@
-
-describe('/admin routes', function(){
-
-  it('Should return 405 unauthorized', function(done){
-    // make request
-    // test resonse
-    done()
-  })
-})
