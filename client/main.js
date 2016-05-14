b40b9172903f1b1f713287c8baf4681f090e1c92
Daithi Coombes <webeire@gmail.com>
2016-05-14 18:54:18 +0100

    add push/toggle responsive sidebar menu

--- client/main.js
+++ client/main.js
@@ -3,20 +3,10 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
+Template.header.events({
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+  // mobile menu button, toggle sidebar
+  'click #menu-toggle': function(){
+    $("#wrapper").toggleClass("toggled");
+  }
 });
