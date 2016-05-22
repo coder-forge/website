5d362bfd7e18d3f229fb3520f7e4a9976b49e4ac
Daithi Coombes <webeire@gmail.com>
2016-05-22 12:39:17 +0100

    install node, export LC_ALL

--- scripts/install_node.sh
+++ scripts/install_node.sh
@@ -2,5 +2,5 @@
 set -e
 
 apt-get install -y build-essential
-curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+(curl -sL https://deb.nodesource.com/setup_6.x | sh)
 apt-get install -y nodejs
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+echo "STARTING coder-forge/website..."
+export LC_ALL=C.UTF-8
 cd /coder-forge/app
 npm install
 meteor run
