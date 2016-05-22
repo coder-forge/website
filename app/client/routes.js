ac9615f12d2a0dd37815d543f54f6119cee49124
Daithi Coombes <webeire@gmail.com>
2016-05-22 14:11:18 +0100

    concept, pages & sidebar links

--- app/client/routes.js
+++ app/client/routes.js
@@ -1,10 +1,45 @@
 FlowRouter.route('/', {
-  action: function(){
+  action: ()=>{
     BlazeLayout.render("mainLayout", {content: "blogHome"});
   }
 });
-FlowRouter.route('/:postId', {
-  action: function(){
-    BlazeLayout.render("mainLayout", {content: "blogPost"});
+FlowRouter.route('/attend', {
+  action: ()=>{
+    BlazeLayout.render("mainLayout", {content: "attend"});
   }
 });
+FlowRouter.route('/announce', {
+  action: ()=>{
+    BlazeLayout.render("mainLayout", {content: "announce"});
+  }
+});
+FlowRouter.route('/tools', {
+  action: ()=>{
+    BlazeLayout.render("mainLayout", {content: "tools"});
+  }
+});
+FlowRouter.route('/resources', {
+  action: ()=>{
+    BlazeLayout.render("mainLayout", {content: "resources"});
+  }
+});
+FlowRouter.route('/competitions', {
+  action: ()=>{
+    BlazeLayout.render("mainLayout", {content: "competitions"});
+  }
+});
+
+routerForge = FlowRouter.group('/forges', {
+  prefix: '/forges',
+  name: 'forges'
+});
+routerForge.route('/', {
+  action: ()=>{
+    BlazeLayout.render("mainLayout", {content: "forges"});
+  }
+});
+routerForge.route('/:forge', {
+  action: ()=>{
+    BlazeLayout.render("mainLayout", {content: "forges/:forge"});
+  }
+})
