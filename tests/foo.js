3438ffd9b1eacdf4807405326075c3207e2da60b
Daithi Coombes <webeire@gmail.com>
2016-06-08 21:08:49 +0100

    end of day - hey!

--- tests/foo.js
+++ tests/foo.js
@@ -1,13 +1,23 @@
 import Users from "../app/imports/lib/Users.js";
 
-const expect = require('chai').expect;
+const expect = require('chai').expect,
+    sinon = require('sinon');
+
 let Meteor;
+
+beforeEach((done)=>{
+    Meteor = {
+
+    }
+    done();
+});
 
 describe("Users", ()=>{
 
     it('gets list of users', ()=>{
 
         let users = new Users(Meteor);
-        expect(true).to.equal(true);
+
+        const usersList = users.list;
     });
 });
