1d61278131f4e70d41fe7e34850305ca7f36c16d
Daithi Coombes <webeire@gmail.com>
2016-12-24 16:36:21 +0000

    bootstrap

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -20,5 +20,11 @@ module.exports = function(defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
+  app.import(app.bowerDirectory + '/bootstrap/dist/css/bootstrap.css');
+  app.import(app.bowerDirectory + '/bootstrap/dist/js/bootstrap.js');
+  app.import(app.bowerDirectory + '/bootstrap/dist/fonts/glyphicons-halflings-regular.woff', {
+    destDir: 'fonts'
+  });
+
   return app.toTree();
 };
