67275f23fbc33b1f43bd1b3aa0d758c8a80081e7
Daithi Coombes <webeire@gmail.com>
2016-12-26 12:58:25 +0000

    concept & tests: meetup component, service

---
+++ tests/unit/controllers/attend-meetup-test.js
@@ -0,0 +1,15 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('controller:attend-meetup', 'Unit | Controller | attend meetup', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+// Replace this with your real tests.
+test('it renders one meetup', function(assert) {
+  let controller = this.subject();
+});
+
+test('it renders multiple meetups', function(assert){
+
+});
---
+++ tests/unit/services/api-meetup-test.js
@@ -0,0 +1,19 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('service:api-meetup', 'Unit | Service | api meetup', {
+  // Specify the other units that are required for this test.
+  // needs: ['service:foo']
+});
+
+// Replace this with your real tests.
+test('it will authenticate', function(assert){
+    let service = this.subject();
+});
+
+test('it will get all meetups', function(assert){
+    let service = this.subject();
+});
+
+test('it will search by geolocation', function(assert){
+    let service = this.subject();
+});
