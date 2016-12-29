4a3ce58fd0a0bc2f10c077ecfdea9030b1511aa1
Daithi Coombes <webeire@gmail.com>
2016-12-29 21:07:45 +0000

    list next 20 events

--- app/routes/attend.js
+++ app/routes/attend.js
@@ -2,6 +2,13 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     model(){
-        return $.getJSON('https://api.meetup.com/Dublin-Coder-Forge/events?photo-host=public&page=20&sig_id=183789567&sig=0c247a6dff1ac1cda1f65930c1923a3353074c30');
+        return Ember.$.getJSON('https://api.meetup.com/Dublin-Coder-Forge/events?photo-host=public&page=20&sig_id=183789567&sig=0c247a6dff1ac1cda1f65930c1923a3353074c30')
+            .then(res => {
+                for(let x=0; x<res.length; x++){
+                    res[x].time = new Date(res[x].time); //unix_timestamp*1000);
+                }
+
+                return res;
+            });
     }
 });
---
+++ tests/acceptance/attend-test.js
@@ -0,0 +1,17 @@
+import { test } from 'qunit';
+import moduleForAcceptance from 'coderforge-website/tests/helpers/module-for-acceptance';
+
+moduleForAcceptance('Acceptance | attend');
+
+test('visiting /attend', function(assert) {
+    visit('/attend');
+
+    andThen(function() {
+        assert.equal(find('ul#event-list li:first dt.name:Contains("Name")').length, 1);
+        assert.equal(find('ul#event-list li:first dt.host:Contains("Host")').length, 1);
+        assert.equal(find('ul#event-list li:first dt.address:Contains("Address")').length, 1);
+        assert.equal(find('ul#event-list li:first dt.time:Contains("Time")').length, 1);
+        assert.equal(find('ul#event-list li.event').length, 20);
+        assert.equal(currentURL(), '/attend');
+    });
+});
