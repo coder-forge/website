5c30951ad97e2daf4f2f6640cd5f2046bf098ca2
a_kustera <antonioktr@gmail.com>
2017-02-20 20:34:12 +0000

    moved assert current URL to the top of the function in attend-tests.js

--- tests/acceptance/attend-test.js
+++ tests/acceptance/attend-test.js
@@ -7,12 +7,12 @@ test('visiting /attend', function(assert) {
     visit('/attend');
 
     andThen(function() {
+        assert.equal(currentURL(), '/attend');
         assert.equal(find('ul#event-list li:first img').length, 1);
         assert.equal(find('ul#event-list li:first dt.name:Contains("Name")').length, 1);
         assert.equal(find('ul#event-list li:first dt.host:Contains("Host")').length, 1);
         assert.equal(find('ul#event-list li:first dt.address:Contains("Address")').length, 1);
         assert.equal(find('ul#event-list li:first dt.time:Contains("Time")').length, 1);
         assert.equal(find('ul#event-list li.event').length, 20);
-        assert.equal(currentURL(), '/attend');
     });
 });
