02cb97cb8abc4a111c4a3d68e4a218ff1ec675f6
EndaMoore <endamoore@gmail.com>
2017-03-06 20:41:01 +0000

    photo

---
+++ app/controllers/gallery.js
@@ -0,0 +1,12 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+
+    images: [
+        {
+            thumb:"https://www.dropbox.com/s/9bvedjem4luwfng/20161005_201003.jpg?dl=0",
+            src:"https://www.dropbox.com/s/9bvedjem4luwfng/20161005_201003.jpg?dl=0",
+            title: "anything for now"
+        }
+    ]
+});
--- app/routes/gallery.js
+++ app/routes/gallery.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
+    images(){
+        return[
+            {
+                thumb:"https://www.dropbox.com/s/9bvedjem4luwfng/20161005_201003.jpg?dl=0",
+                src:"https://www.dropbox.com/s/9bvedjem4luwfng/20161005_201003.jpg?dl=0",
+                title: "anything for now"
+            }
+        ];
+    }
 });
